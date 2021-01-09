482238a1bdec5ec24c56cbaa66e655a789714421
daveratcliffe <64509682+daveratcliffe@users.noreply.github.com>
2021-01-09 13:17:34 -0500

    init push

---
+++ client/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Main = React.createClass({
+  render() {
+    return (
+      <div>
+        <Link to="/">Shinstagram</Link>
+      </div>
+    )
+  }
+});
+
+export default Main;
---
+++ client/instaclone.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom'
+import css from './styles/style.styl';
+
+//Import the components
+import Main from './components/Main'
+
+
+render(<p>hello</p>, document.getElementById('root'));
